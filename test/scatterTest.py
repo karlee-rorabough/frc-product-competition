6c3e842ef73a746b7ac16d6a9058e4415e2a9a43
karleerorabough <kaudreyr@gmail.com>
2020-11-25 19:43:42 -0800

    Started working with the plotly dashboard & created a download requirements list.

---
+++ test/scatterTest.py
@@ -0,0 +1,33 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import plotly.express as px
+
+df = px.data.iris()
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div([
+    dcc.Graph(id="scatter-plot"),
+    html.P("Petal Width:"),
+    dcc.RangeSlider(
+        id='range-slider',
+        min=0, max=2.5, step=0.1,
+        marks={0: '0', 2.5: '2.5'},
+        value=[0.5, 2]
+    ),
+])
+
+@app.callback(
+    Output("scatter-plot", "figure"), 
+    [Input("range-slider", "value")])
+def update_bar_chart(slider_range):
+    low, high = slider_range
+    mask = (df.petal_width > low) & (df.petal_width < high)
+
+    fig = px.scatter_3d(df[mask], 
+        x='sepal_length', y='sepal_width', z='petal_width',
+        color="species", hover_data=['petal_width'])
+    return fig
+app.run_server(debug=True)
